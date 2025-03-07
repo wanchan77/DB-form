0b2b83887838ec2ff2d29064df4052aa0e490466
wanchan77 <zhende.wang@grn-ai.com>
2025-03-07 13:59:30 +0900

    Made formulas

--- app.py
+++ app.py
@@ -207,10 +207,10 @@ elif st.session_state["page"] == "page2A":
         # 燃料取得
         fuel = st.session_state["user_input"].get("燃料", "")
 
-        # 事前定義された値
+        # **事前定義された値**
         predefined_values = [
-            ("電気の排出係数", 0.000434 if fuel == "電力" else None, "t-CO2/kWh", "・環境省令和5年：0.000434(t-CO2/kWh)\nhttps://ghg-santeikohyo.env.go.jp/files/calc/r05_coefficient_rev4.pdf\n\n・環境省：0.000488(t-CO2/kWh)\n環境省のエクセル" if fuel == "電力" else ""),
-            ("電気料金", 22.97 if fuel == "電力" else None, "円/kWh", "・新電力ネット(高圧)22.97(円/kWh)\nhttps://pps-net.org/unit\n\n・環境省：12.1587 (円/kWh)\n環境省のエクセル" if fuel == "電力" else ""),
+            ("電気の排出係数", 0.000434 if fuel == "電力" else None, "t-CO2/kWh", "・環境省令和5年：0.000434(t-CO2/kWh)\nhttps://ghg-santeikohyo.env.go.jp/files/calc/r05_coefficient_rev4.pdf" if fuel == "電力" else ""),
+            ("電気料金", 22.97 if fuel == "電力" else None, "円/kWh", "・新電力ネット(高圧)22.97(円/kWh)\nhttps://pps-net.org/unit" if fuel == "電力" else ""),
             ("想定稼働年数", 10, "年", "")
         ]
 
@@ -219,25 +219,20 @@ elif st.session_state["page"] == "page2A":
             
             if fuel != "電力" and name in ["電気の排出係数", "電気料金"]:
                 name_display = "燃料が電力ではありません"
-                value = 0.0  # 電力でない場合、数値は 0.0 に設定
+                value = None
             else:
                 name_display = name
             
-            st.session_state["user_input"].setdefault(f"規定値_{name}_名前", name_display)
-            st.session_state["user_input"].setdefault(f"規定値_{name}_数字", value)
-            st.session_state["user_input"].setdefault(f"規定値_{name}_単位", unit if value is not None else "")
-            st.session_state["user_input"].setdefault(f"規定値_{name}_説明", description if value is not None else "")
-            
-            st.text_input(f"規定値({name})の名前", value=st.session_state["user_input"][f"規定値_{name}_名前"])
+            st.text_input(f"規定値({name})の名前", value=name_display)
             st.number_input(
                 f"規定値({name})の数字",
                 min_value=0.0,
                 step=0.000001 if name == "電気の排出係数" else 0.01,
                 format="%.6f" if name == "電気の排出係数" else "%.2f",
-                value=st.session_state["user_input"][f"規定値_{name}_数字"]
+                value=0.0 if value is None else value
             )
-            st.text_input(f"規定値({name})の単位", value=st.session_state["user_input"][f"規定値_{name}_単位"])
-            st.text_area(f"規定値({name})の説明", value=st.session_state["user_input"][f"規定値_{name}_説明"])
+            st.text_input(f"規定値({name})の単位", value=unit if value is not None else "")
+            st.text_area(f"規定値({name})の説明", value=description if value is not None else "")
 
         # **追加の規定値 13個**
         for i in range(13):
